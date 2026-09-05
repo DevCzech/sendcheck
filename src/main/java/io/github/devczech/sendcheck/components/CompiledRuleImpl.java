90ca4533007814abc167646db95684a12a99dab1
DevCzech <dev_czech@email.cz>
2026-09-06 00:38:17 +0200

    Added better rule file format and parsing

--- src/main/java/io/github/devczech/sendcheck/components/CompiledRuleImpl.java
+++ src/main/java/io/github/devczech/sendcheck/components/CompiledRuleImpl.java
@@ -2,6 +2,7 @@ package io.github.devczech.sendcheck.components;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class CompiledRuleImpl implements CompiledRule {
     private final String name;
@@ -15,11 +16,16 @@ public class CompiledRuleImpl implements CompiledRule {
     }
 
     public static CompiledRuleImpl parse(String ruleLine) {
-        String[] split = ruleLine.split(",");
-        if (split.length == 3) {
-            return new CompiledRuleImpl(split[0].strip(), split[1].strip(), Pattern.compile(split[2].strip()));
-        } else {
-            throw new IllegalArgumentException("Could not parse rule line");
+        try {
+            final int firstSpaceIndex = ruleLine.indexOf(' ');
+            final String name = ruleLine.substring(0, firstSpaceIndex);
+            final int secondSpaceIndex = ruleLine.indexOf(' ', firstSpaceIndex + 1);
+            final String category = ruleLine.substring(firstSpaceIndex + 1, secondSpaceIndex);
+            final String pattern = ruleLine.substring(secondSpaceIndex + 1);
+
+            return new CompiledRuleImpl(name, category, Pattern.compile(pattern));
+        } catch (PatternSyntaxException | StringIndexOutOfBoundsException | NullPointerException e) {
+            throw new IllegalArgumentException("Could not parse rule line", e);
         }
     }
 
